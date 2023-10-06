7979116619c75fd1e0f05f1bbab7e23a373f7cc5
JoraAbovyan <jora.abovyan@tumo.org>
2023-10-06 21:27:55 +0400

    inharitance

--- grasseater.js
+++ grasseater.js
@@ -1,4 +1,4 @@
-class GrassEater {
+class GrassEater  {
     constructor(x, y, index) {
         this.x = x;
         this.y = y;
--- script.js
+++ script.js
@@ -1,21 +1,50 @@
 var matrix = [];
-let characterCount = 5;
 let side = 20
 let GrassArr = []
 let GrassEaterArr = []
 let PredatorArr = []
 let EaterArr = []
 let HumanArr = []
+const sideCount = 16;
 // HI
 function setup() {
-    MatrixGenerator()
+    matrixG(sideCount)
+    Generation(30, 1);
+    Generation(10, 2);
     createCanvas(side * matrix[0].length, side * matrix.length);
     background('#acacac');
-    frameRate(10)
+    frameRate(2)
 
 }
 
+
+
+function matrixG(a)
+{
+    for(let i = 0; i<a;i++){
+        matrix.push([])
+        for(let j = 0; j<a;j++){
+            matrix[i].push(0)
+            
+        }
+    }
+}
+
+function Generation(count, character) {
+    let p = 0;
+    while (p < count) {
+        let k = Math.floor(random(0, sideCount ))
+        let l = Math.floor(random(0, sideCount ))
+        if (matrix[k][l] == 0) {
+            matrix[k][l] = character
+        }
+        p++;
+    }
+}
+
 function draw() {
+
+
     for (let y = 0; y < matrix.length; y++) {
         for (let x = 0; x < matrix[y].length; x++) {
             if (matrix[y][x] == 1) {
@@ -24,7 +53,7 @@ function draw() {
             } else if (matrix[y][x] == 2) {
                 fill("yellow")
                 rect(side * x, side * y, side, side)
-            }else if (matrix[y][x] == 3) {
+            } else if (matrix[y][x] == 3) {
                 fill("cyan")
                 rect(side * x, side * y, side, side)
             }
@@ -73,29 +102,23 @@ function draw() {
             }
         }
 
-    } 
+    }
     for (let i = 0; i < GrassArr.length; i++) {
         GrassArr[i].mull()
     }
     for (let i = 0; i < GrassEaterArr.length; i++) {
         GrassEaterArr[i].eat()
     }
-    for (let i = 0; i < PredatorArr.length; i++) {
-        PredatorArr[i].eat()
-    }
-    for (let i = 0; i < EaterArr.length; i++) {
-        EaterArr[i].eat()
-    }
-    for (let i = 0; i < HumanArr.length; i++) {
-        HumanArr[i].eat()
-    }
+    // for (let i = 0; i < PredatorArr.length; i++) {
+    //     PredatorArr[i].eat()
+    // }
+    // for (let i = 0; i < EaterArr.length; i++) {
+    //     EaterArr[i].eat()
+    // }
+    // for (let i = 0; i < HumanArr.length; i++) {
+    //     HumanArr[i].eat()
+    // }
 }
 
-function MatrixGenerator() {
-    for (var y = 0; y < side; ++y) {
-        matrix[y] = [];
-        for (var x = 0; x < side; ++x) {
-            matrix[y][x] = Math.round(random(0, characterCount));
-        }
-    }
-}
+
+
